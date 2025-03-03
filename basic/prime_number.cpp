4a624277dd428a2cc4d750e34c98e70a33757644
ujjwalgoel23 <ujjwalgoel2711@gmail.com>
2025-03-03 09:27:35 +0530

    first

--- basic/prime_number.cpp
+++ basic/prime_number.cpp
@@ -22,7 +22,7 @@ int main(){
     // for(int i=1;i*i<=n;i++){
     //    if(n%i==0) {
     //     count++;
-    //       if((n/i) != i) count++;
+    //      if((n/i) != i) count++;
     // }
     // }
     // if(count==2)  cout<<"prime number";
